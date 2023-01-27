1b22e8e04ad17925e870164dc1adc69b79b209d8
bezbrain <belloquadriolawale@gmail.com>
2023-01-27 00:27:56 -0800

    Completed the JS to effect the functionality

--- main.js
+++ main.js
@@ -1,15 +1,36 @@
-function isPrime(num) {
+function isPrimeNum(num) {
   if (num < 2) {
     return "I am not prime";
   }
   for (let i = 2; i <= Math.sqrt(num); i++) {
-    console.log("Testing");
+    console.log(i);
     if (num % i === 0) {
       return "I am not prime";
     }
   }
   return "I am a prime number";
 }
-// isPrime();
+console.log(isPrimeNum(53));
 
-console.log(isPrime(5));
+// The DOM
+const input = document.querySelector("input");
+const btn = document.querySelector(".check-btn");
+const result = document.querySelector(".result-value");
+
+console.log(input, btn, result);
+
+btn.onclick = () => {
+  isPrime(input.value);
+};
+
+const isPrime = (num) => {
+  if (num < 2) {
+    return (result.textContent = "not prime number");
+  }
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return (result.textContent = "not prime number");
+    }
+  }
+  return (result.textContent = "prime number");
+};
