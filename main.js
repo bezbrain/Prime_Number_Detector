7e8752e212e18689b1a614653a089750cbfec4cc
bezbrain <belloquadriolawale@gmail.com>
2023-01-27 01:39:34 -0800

    Did some texts arrangement

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-function isPrimeNum(num) {
-  if (num < 2) {
-    return "I am not prime";
-  }
-  for (let i = 2; i <= Math.sqrt(num); i++) {
-    console.log(i);
-    if (num % i === 0) {
-      return "I am not prime";
-    }
-  }
-  return "I am a prime number";
-}
-console.log(isPrimeNum(53));
+// function isPrimeNum(num) {
+//   if (num < 2) {
+//     return "I am not prime";
+//   }
+//   for (let i = 2; i <= Math.sqrt(num); i++) {
+//     console.log(i);
+//     if (num % i === 0) {
+//       return "I am not prime";
+//     }
+//   }
+//   return "I am a prime number";
+// }
+// console.log(isPrimeNum(53));
 
 // The DOM
 const input = document.querySelector("input");
